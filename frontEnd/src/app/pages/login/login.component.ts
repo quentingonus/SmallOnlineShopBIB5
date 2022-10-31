ae39dfc1d6e6a9b7252b51f324ef123ff22d729d
Hein Htet Aung <heinhtetzaung26698@gmail.com>
2022-10-31 16:44:41 +0630

    commit for login page

--- frontEnd/src/app/pages/login/login.component.ts
+++ frontEnd/src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  loginUser(e : any) {
+    e.preventDefault();
+    console.log(e);
+    var username = e.target.elements[0].value;
+    var password = e.target.elements[1].value;
+    
+    if (username == 'Admin' && password == 'admin123') {
+      this.router.navigate(['home']);
+    }
   }
 
 }
