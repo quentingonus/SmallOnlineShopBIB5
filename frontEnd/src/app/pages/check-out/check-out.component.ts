e2f623d5e0bd3b731aa4e44677992afe5ceefd1a
Phyo Thiha Kyaw <67636515+PTHK0805@users.noreply.github.com>
2022-11-06 19:10:36 +0630

    error.

--- frontEnd/src/app/pages/check-out/check-out.component.ts
+++ frontEnd/src/app/pages/check-out/check-out.component.ts
@@ -1,3 +1,5 @@
+import { CartService } from './../../services/cart.service';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckOutComponent implements OnInit {
 
-  constructor() { }
+  form;
+  cartItem: any;
+  constructor(private fb: FormBuilder, private cartService: CartService) {
+    this.form = fb.group({
+      fname: ['', Validators.required],
+      lname: ['', Validators.required],
+      username: ['', Validators.required],
+      email: [],
+      address1: ['', Validators.required],
+      address2: [],
+      country: ['', Validators.required],
+      state: ['', Validators.required],
+      zip: ['', Validators.required],
+      sameaddress: [],
+      remember: [],
+      payment: ['', Validators.required],
+      card_name: ['', Validators.required],
+      card_number: ['', Validators.required]
+    });
+
+  }
+
+  onSubmit() {
+    console.log(this.form)
+  }
+
+  totalCartItem() {
+    console.log('From Method:',this.cartItem)
+    if (!this.cartItem) {
+      return 0
+    }
+    let totalAmount = 0
+    this.cartItem.map((item: any) => {
+      totalAmount += item.amount
+    })
+    return totalAmount
+  }
 
   ngOnInit(): void {
+     this.cartService.getCart().subscribe((item: any) => {
+       this.cartItem = item;
+       console.log(item);
+     });
   }
 
 }
