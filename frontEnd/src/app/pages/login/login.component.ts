c4ab4f81480ee44a095cc81361526507ab9b1ab7
Hein Htet Aung <heinhtetzaung26698@gmail.com>
2022-11-08 15:36:59 +0630

    'auth-guard'

--- frontEnd/src/app/pages/login/login.component.ts
+++ frontEnd/src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { HomeComponent } from '../home/home.component';
 
@@ -12,27 +13,43 @@ export class LoginComponent implements OnInit {
   username = "";
   password = "";
   errorMsg = "";
+  loginForm!: FormGroup;
 
-  constructor(private router: Router, private auth: AuthService) { }
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
   }
 
   login() {
-    if (this.username.trim().length === 0) {
-      this.errorMsg = "Username is required";
-    } else if (this.password.trim().length === 0) {
-      this.errorMsg = "Password is required";
-    } else {
-      this.errorMsg = "";
-      let res = this.auth.login(this.username, this.password);
-      if (res === 200) {
-        this.router.navigate(['home']);
-      }
-      if (res === 403) {
-        this.errorMsg = "Invalid Credentials";
-      }
+    //if (this.username.trim().length === 0) {
+    //  this.errorMsg = "Username is required";
+    //} else if (this.password.trim().length === 0) {
+    //  this.errorMsg = "Password is required";
+    //} else {
+    //  this.errorMsg = "";
+    //  let res = this.authService.login(this.username, this.password);
+    //  if (res === 200) {
+    //    this.router.navigate(['home']);
+    //  }
+    //  if (res === 403) {
+    //    this.errorMsg = "Invalid Credentials";
+    //  }
+    //}
+
+    const payload = {
+      username: this.loginForm.controls['username'].value,
+      password: this.loginForm.controls['password'].value
     }
+    this.authService.login(payload).then((dist: any) => {
+      localStorage.setItem('token', dist.token);
+      localStorage.setItem('userLoginData', JSON.stringify(dist.user));
+      this.router.navigate(['home']);
+    })
   }
   
   forget() {
--- frontEnd/src/app/services/auth.service.ts
+++ frontEnd/src/app/services/auth.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable, Subject, throwError, lastValueFrom, Observer } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
@@ -7,18 +10,38 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
 
-  constructor(private router: Router,) { }
+  protected authUserSubject = new Subject<any>();
+  authUser$: Observable<any> = this.authUserSubject.asObservable();
 
-  login(uname : string, pword : string) {
-    if ( uname === 'Admin' && pword === 'admin123') {
-      return 200;
+  constructor(private router: Router, private http: HttpClient) { }
+  
+  async isLoggedIn() {
+    if (localStorage.getItem('userLoginData')) {
+      await this.authUserSubject.next(localStorage.getItem('userLoginData'));
     } else {
-      return 403;
+      await this.authUserSubject.next(null);
     }
   }
 
-  register() {
-    
+  //login(uname : string, pword : string) {
+  //  if ( uname === 'Admin' && pword === 'admin123') {
+  //    return 200;
+  //  } else {
+  //    return 403;
+  //  }
+  //}
+  public login(payload: any) {
+     return lastValueFrom(this.http.post(`${environment.apiUrl}/login`,payload))
+   }
+  
+
+  public reset(payload: any) : Promise<any> {
+    return lastValueFrom(this.http.get(`${environment.apiUrl}/forgot-password`, payload))
+  }
+
+  public resetPasswordUpdate(id: string, token: string, payload: any) : Promise <any>{
+    return lastValueFrom(this.http.post(`${environment.apiUrl}/password-reset-update/${id}/${token}`,payload))
   }
   
 }
+  
