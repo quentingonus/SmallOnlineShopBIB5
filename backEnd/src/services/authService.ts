0643e407862e121502d6af1ad5812236c6081a8c
Furt1v3 <nyanlinhtoo181199@gmail.com>
2022-11-10 15:56:41 +0630

    commit for cart_update and Not_complete_Api_doc

--- backEnd/src/services/authService.ts
+++ backEnd/src/services/authService.ts
@@ -8,12 +8,12 @@ import crypto from "crypto";
 import PasswordReset from '../models/passwordReset';
 import sendEmail from '../utils/sendEmail'
 
-export const loginService = async (req:Request, res:Response) => {
+export const loginService = async (req: Request, res: Response) => {
   User.findOne({ email: req.body.email }).then(async (user: any) => {
     if (!user) {
       return res.status(404).send({
-        success : false ,
-        msg : "Could not find user"
+        success: false,
+        msg: "Could not find user"
       })
     }
 
@@ -46,26 +46,27 @@ export const logoutService = (req: any, res: Response) => {
 
 export const forgetPasswordService = async (req: any, res: Response) => {
   try {
-    const user = await User.findOne({ email : req.body.email});
+    const user = await User.findOne({ email: req.body.email });
     if (!user) {
       return res.status(404).send("Email doesn't exist");
     }
-    let token = await PasswordReset.findOne({ email : req.body.email})
+    let token = await PasswordReset.findOne({ email: req.body.email })
     if (!token) {
       token = await new PasswordReset({
-        email : req.body.email ,
+        email: req.body.email,
         token: crypto.randomBytes(16).toString("hex"),
       }).save();
     }
     const link = `${process.env.BASE_URL}/forget_password_update/${user._id}/${token.token}`;
-    await sendEmail(user.email, "Password Reset" ,link);
-    
+    await sendEmail(user.email, "Password Reset", link);
+
     return res.status(200).json({
-      msg : "Password Reset link sent to your email"
+      msg: "Password Reset link sent to your email"
     })
   } catch (err) {
+    console.log(err);
     return res.send("An Error occured in passwordReset");
-    console.log(err);   
+
   }
 }
 
@@ -76,22 +77,22 @@ export const resetPasswordService = async (req: Request, res: Response) => {
       return res.status(404).send("UserId does not exist");
     }
     const passwordReset = await PasswordReset.findOne({
-      token : req.params.token
+      token: req.params.token
     });
 
     if (!passwordReset) {
       return res.status(404).send("Invalid link or expired");
     }
-    
+
     user.password = await bcrypt.hash(req.body.password, 12);
     await user.save();
     await passwordReset.delete();
 
-    return res.json ({ 
-      msg:'Password reset Successfully'
+    return res.json({
+      msg: 'Password reset Successfully'
     })
   } catch (err) {
-    return res.send ("Password Reset Failed")
+    return res.send("Password Reset Failed")
   }
 };
 
@@ -105,10 +106,10 @@ export const passwordChangeService = async (req: Request, res: Response) => {
         })
       }
 
-      const token= req.params.token;
+      const token = req.params.token;
       if (!token) return res.status(401).send("Unauthorized");
 
-  
+
       if (!compareSync(req.body.oldPassword, user.password)) {
         return res.send({
           success: false,
@@ -116,8 +117,8 @@ export const passwordChangeService = async (req: Request, res: Response) => {
         });
       }
 
-      if(compareSync(req.body.newPassword, user.password)) {
-        return res.send({
+      if (compareSync(req.body.newPassword, user.password)) {
+        return res.status(401).send({
           success: false,
           message: 'Current Password and New Password are same.'
         });
@@ -125,7 +126,7 @@ export const passwordChangeService = async (req: Request, res: Response) => {
 
       user.password = await bcrypt.hash(req.body.newPassword, 12);
       await user.save();
-      return res.json({ message: "Password Change Successfully!" });
+      return res.status(200).json({ message: "Password Change Successfully!" });
     })
   } catch (error) {
     res.send("An error occured");
