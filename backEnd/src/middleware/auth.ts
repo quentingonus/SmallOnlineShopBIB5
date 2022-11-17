a0b2d7a5e042fd93816d6b51642b9a8d3e809689
Quentin Gonus <quentingonus@gmail.com>
2022-11-18 02:58:46 +0630

    Changed JWT

--- backEnd/src/middleware/auth.ts
+++ backEnd/src/middleware/auth.ts
@@ -1,13 +1,11 @@
 const jwt = require("jsonwebtoken");
 
 export const verifyToken = (req: any, res: any, next: any) => {
-  //const token = req.headers["x-access-token"];
-  let token = req.headers['authorization'].replace("Bearer ", "");
-
-  if (!token) {
-    return res.status(403).send("A token is required for authentication");
-  }
   try {
+    let token = req.headers['authorization'].replace("Bearer ", "");
+    if (!token) {
+      return res.status(403).send("A token is required for authentication");
+    }
     req.decoded = jwt.verify(token, (process.env.JWT_SECRET as any));
   } catch (err) {
     return res.status(401).send("Invalid Token");
